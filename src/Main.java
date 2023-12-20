864498a21bf1f9f9d0056dd3a7c92e7b54743efc
anamariaow <new_annemarie@yahoo.com>
2023-12-20 14:48:04 +0100

    HashMap dove si prendono i valori, si ordinano e si stampa

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        //Creare un hashmap,
+        Map<String, String> capitali = new HashMap<>();
+
+        capitali.put("Luca", "Roma");
+        capitali.put("Inghilterra", "Londra");
+        capitali.put("Germania", "Berlino");
+        capitali.put("Francia", "Parigi");
+
+        System.out.println("Capitali: " + capitali);
+
+        // prendere tutti i valori,
+        Collection<String> values = capitali.values();
+        System.out.println("Values: " + values);
+
+        //ordinarli e stamparli
+        ArrayList<String> ordinati = new ArrayList<>(values);
+        ordinati.sort(Comparator.naturalOrder());
+        System.out.println("Lista ordinata: " + ordinati);
+    }
+}
